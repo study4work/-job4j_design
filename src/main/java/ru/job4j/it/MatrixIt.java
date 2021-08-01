c502363bbd806f4b78dab810cca2a2f2f8b24e9f
study4work <FaJlJleN@ya.ru>
2021-08-01 16:48:13 +0300

    delete .idea and *.iml

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -1,2 +1,37 @@
-package ru.job4j.it;public class MatrixIt {
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean rsl = false;
+        if (column < data[row].length) {
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (column < data[row].length) {
+            column++;
+        } else {
+            row++;
+            column = 0;
+        }
+        return data[row][column];
+    }
 }
