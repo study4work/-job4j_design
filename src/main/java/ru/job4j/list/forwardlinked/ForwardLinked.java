06bdc682e3cb51e955b72b6ee284c916673799b1
FaJlJleN@yandex.ru <FaJlJleN@ya.ru>
2021-08-29 19:31:27 +0300

    3. Удалить head в односвязном списке.

--- src/main/java/ru/job4j/list/forwardlinked/ForwardLinked.java
+++ src/main/java/ru/job4j/list/forwardlinked/ForwardLinked.java
@@ -6,6 +6,11 @@ import java.util.NoSuchElementException;
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
 
+    public void addFirst(T value) {
+        Node<T> newFirst = new Node<>(value, head);
+        head = newFirst;
+    }
+
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
---
+++ src/main/java/ru/job4j/list/forwardlinked/SimpleStack.java
@@ -0,0 +1,16 @@
+package ru.job4j.list.forwardlinked;
+
+import java.util.Iterator;
+
+public class SimpleStack<T> {
+
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public T pop() {
+        return linked.deleteFirst();
+    }
+
+    public void push(T value) {
+        linked.addFirst(value);
+    }
+}
