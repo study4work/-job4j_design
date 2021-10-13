8c5c68b33b40430c4fa44105e698584fb9c3fef1
FaJlJleN@yandex.ru <FaJlJleN@ya.ru>
2021-10-13 23:46:25 +0300

    исправления

--- src/main/java/ru/job4j/generics/store/User.java
+++ src/main/java/ru/job4j/generics/store/User.java
@@ -1,8 +1,6 @@
 package ru.job4j.generics.store;
 
 public class User extends Base {
-    // класс User наследуется от Base,т.к. у Base есть поле String id,
-    // модель User иницилизируется полем id класса Base
     public User(String id) {
         super(id);
     }
