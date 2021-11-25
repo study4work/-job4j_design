56501dafbee117029d97c23e7cd14c53c4cec4c9
FaJlJleN@yandex.ru <FaJlJleN@ya.ru>
2021-11-25 21:47:47 +0300

    2. Статистика по коллекции.

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -8,9 +8,9 @@ public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info log = new Info(0, 0, 0);
-        int c = 0;
-        int a = 0;
-        int u = 0;
+        int changeIncrement = 0;
+        int addIncrement = 0;
+        int unmodifyIncrement = 0;
         Map<Integer, String> rsl = Stream.of(previous)
                 .flatMap(Set::stream)
                 .collect(Collectors.toMap(
@@ -19,18 +19,20 @@ public class Analize {
 
             for (User user : current) {
                 if (!rsl.containsKey(user.getId())) {
-                    a++;
+                    addIncrement++;
                     continue;
                 }
                 if (!rsl.get(user.getId()).equals(user.getName())) {
-                    c++;
+                    changeIncrement++;
                     continue;
                 }
-                u++;
+                unmodifyIncrement++;
             }
-            log.setAdded(a);
-            log.setChanged(c);
-            log.setDeleted((current.size() + previous.size()) - a - u * 2 - c * 2);
+            log.setAdded(addIncrement);
+            log.setChanged(changeIncrement);
+            log.setDeleted((current.size() + previous.size())
+                    - addIncrement - unmodifyIncrement
+                    * 2 - changeIncrement * 2);
         return log;
     }
 }
